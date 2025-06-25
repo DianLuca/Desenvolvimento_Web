276094cdf4426ca51540ce6aca93c456ab3dd8b4
DianLuca <dianlucca@hotmail.com>
2025-06-24 21:08:30 -0300

    Concluindo o desenvolvimento das atividades

--- JS/Atividade004/js/script.js
+++ JS/Atividade004/js/script.js
@@ -169,3 +169,90 @@ verificar.addEventListener('click', ()=> {
 
     calcular_passagem(valor || 0);
 });
+
+// Atividade F
+
+function verificar_bissexto(valor) {
+    if (valor <= 0) {
+        resultado.textContent = 'Insira um valor maior que 0.'
+    } else {
+        if ((valor % 4 == 0) && (valor % 100 != 0) || (valor % 400 == 0)){
+            resultado.textContent = `O ano ${valor} é um ano bissexto!`
+        } else {
+            resultado.textContent = `O ano ${valor} não é um ano bissexto!`
+        }
+    }
+};
+
+verificar.addEventListener('click', () => {
+    let ano = document.querySelector('#ano');
+
+    let valor = parseInt(ano.value) || 1;
+
+    verificar_bissexto(valor);
+});
+
+// Atividade G 
+
+function verificar_triangulo(valor, valor1, valor2) {
+    if (valor <= 0 && valor1 <= 0 && valor2 <= 0) {
+        resultado.textContent = 'Insira um valor maior que 0.';
+    } else {
+
+        if ((valor <= 0) || (valor1 <= 0) || (valor2 <= 0)) {
+            resultado.textContent = `A soma dos segmentos: ${valor}, ${valor1} e ${valor2} não pode ser um triângulo.`;
+        } else if ((valor < (valor1 + valor2)) && (valor1 < (valor + valor2)) && (valor2 < (valor1 + valor))){
+            resultado.textContent = `A soma dos segmentos: ${valor}, ${valor1} e ${valor2} pode ser um triângulo.`;
+        } else {
+            resultado.textContent = `A soma dos segmentos: ${valor}, ${valor1} e ${valor2} não pode ser um triângulo.`;
+        };
+        
+    };
+};
+
+verificar.addEventListener('click', () => {
+    let segmento = document.querySelector('#segmento');
+    let segmento1 = document.querySelector('#segmento1');
+    let segmento2 = document.querySelector('#segmento2');
+
+    let valor = parseInt(segmento.value) || 0;
+    let valor1 = parseInt(segmento1.value) || 0;
+    let valor2 = parseInt(segmento2.value) || 0;
+
+    verificar_triangulo(valor, valor1, valor2);
+});
+
+
+// Atividade H
+
+function resolver_raiz (valor, valor1, valor2) {
+    let delta = (valor1**2) - 4*valor*valor2;
+
+    let raiz_delta = delta ** (1/2);
+    // let raiz_delta = Math.sqrt(delta);
+
+    if (delta < 0) {
+
+        resultado.textContent = 'Não possuí uma raiz real.';
+
+    } else {
+
+        let x1 = (-valor1 + raiz_delta)/(2*valor);
+
+        let x2 = (-valor1 - raiz_delta)/(2*valor);
+
+        resultado.textContent = `O valor das raízes será igual a: ${x1} e ${x2}.`;
+    };
+};
+
+verificar.addEventListener('click', () => {
+    let valorA = document.querySelector('#valorA');
+    let valorB = document.querySelector('#valorB');
+    let valorC = document.querySelector('#valorC');
+
+    let valor = parseInt(valorA.value) || 1;
+    let valor1 = parseInt(valorB.value) || 1;
+    let valor2 = parseInt(valorC.value) || 1;
+
+    resolver_raiz(valor, valor1, valor2);
+});
